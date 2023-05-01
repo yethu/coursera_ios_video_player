be7f66777b21bceee049dc19ece913b20e8dac85
Ye Thu <yethu.personal@gmail.com>
2023-05-01 14:17:13 +0700

    Initial Commit

---
+++ coursera_ios_course_video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coursera_ios_course_video_player
+//
+//  Created by Ye Thu on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
